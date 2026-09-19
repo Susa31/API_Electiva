4eac90209254980439c33fbf5ee40ae41e056d0d
ValenMejiaC <vmejiaconsuegra@gmail.com>
2026-09-19 07:24:24 -0500

    fix: ajusta tipos en types/api.ts

--- src/types/api.ts
+++ src/types/api.ts
@@ -2,7 +2,6 @@ export interface Meal {
   idMeal: string;
   strMeal: string;
   strMealThumb: string;
-  // Propiedades opcionales que vienen en el detalle
   strCategory?: string;
   strArea?: string;
   strInstructions?: string;
